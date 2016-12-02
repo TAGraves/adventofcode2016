abfb1058c4dd7d6c07682b5fbce368fa815f974d
Tommy Graves <tommy@tagraves.com>
2016-12-02 16:51:17 -0500

    Day 2 part 2

--- day2.js
+++ day2.js
@@ -1,43 +1,73 @@
-const INPUT = `DLRRRRLRLDRRRURRURULRLLULUURRRDDLDULDULLUUDLURLURLLDLUUUDUUUULDRDUUDUDDRRLRDDDUDLDLLRUURDRULUULRLRDULULLRLRLRLDRLUULDLDDDDRRLRUUUDDRURRULLLRURLUURULLRLUDDLDRUULDRURULRRRLLLRDLULDRRDDUDLURURLDULDRDRLDDUURRDUDDRDUURDULDUURDUDRDRULDUDUULRRULUUURDUURUDLDURDLRLURUUDRRDLRUDRULRURLDLLDLLRRDRDRLRRRULDRRLDUURLUUDLUUDDLLRULRDUUDURURLUURDRRRUDLRDULRRRLDRDULRUUDDDLRDUULDRLLDRULUULULRDRUUUULULLRLLLRUURUULRRLDDDRULRRRUDURUR
-RULRUUUDLLUDURDRDDLLRLLUDRUDDRLRRDLDLDRDULDLULURDLUDDDUULURLDRUUURURLLRRDDDUUDRLRLLDLDRDDDRDUDLRDRDLLLDDLDUDDRUDUUDLLLLLDULRLURRRLLURUUULUDRLRLRLURRDRLLLRLLULRLLLDDLRLRDLUUUUUDULULDDULLUDUURDLRUDLRUDLRLLRLDLULRLDUDRURURDLRULDLULULDLLDLDLDLLLUDUDDLRLRRDULLUDRDDLLLDUURDULUDURLLLDRUDDDLRLULDLDRRDDDRDULDDUDRDDULLULRRLRUULRDUDURUDULUDUDURLDRDUUDDRRLRURDRRLRDDDDRUDLUDLDDLRDLUUDLRRURDDLURDLRDLLRDRDLDLDUUUURULUULDDDDLDULUURRRULUDLLLDRULDRURL
-RRRLRDLLDUURDRRRLURDUULUDURDRRUUDURURRLDLLDRDLRRURDDUDDURLRUUDDULULRUUDRLUUDDLLDDDLRRRDLLLLLLRRURDULDLURRURRDDLDDDUDURRDURRRLUDRRULLRULDRLULRULDDRLLRDLRDUURULURLUURLRRULDULULUULDUDLRLDRDDRRRUUULULDUURLRLLURRLURDUUDDDRUULDLLLDRUURLRRLLDDUDRDLDDDULDRDDDUDRRLLLULURDUDLLUUURRLDULURURDDLUDLLRLDRULULURDLDRLURDLRRDRRUULLULDLURRDDUDRDDDLDUDLDRRUDRULDLDULRLLRRRRDDRLUURRRRDDLLRUURRLRURULDDULRLULRURRUULDUUDURDRRLRLUDRULDRUULUUDRDURDURRLULDDDULDDLRDURRUUUUUDDRRDLRDULUUDDL
-DRRLLRRLULDDULRDDLRLDRURDDUDULURRDLUUULURRRLLRLULURLLRLLDLLUDDLLRDRURRDLDDURRURDRDDUDDDLLRLDLDLDDDDRRRRUDUDLRDUDDURLLRURRDUDLRLLUDDRLDUUDDLLLUDRRRLLDDULUDDRLLUDDULLDDLRLDLRURRLUDDLULULDLUURDLLUDUDRRRRDULUDLRRLRUDDUUDRRLLRUUDRRLDDLRRRUDRRDRRDDUDLULLURRUURLLLDRDDLUDDDUDDRURURDLRUULLRDRUUDRDUDRLULLDURUUULDDLDRDRUDRUDUULDDRLRDRRDRRRRLRLRUULDDUUDDLLLLRRRDUDLRDLDUDDUURLUDURLDRRRDRUDUDRLDLRLDRDDLUDRURLRDRDLDUDDDLRLULLUULURLDDDULDUDDDLDRLDLURULLUDLLDRULDLLLDUL
-LDULURUULLUDLDDRLLDURRULRLURLLURLRRLRDLDDRUURULLRUURUURRUDDDLRRLDDLULDURLLRDURDLLLURLDRULLURLRLDRDRULURDULDLLDUULLLDUDULDURLUDRULRUUUUUUDUUDDDLLURDLDLRLRDLULRDRULUUDRLULLURLRLDURDRRDUDDDURLLUUDRRURUDLDUDRLRLDRLLLLDLLLURRUDDURLDDRULLRRRRDUULDLUDLDRDUUURLDLLLDLRLRRLDDULLRURRRULDLURLURRRRULUURLLUULRURDURURLRRDULLDULLUDURDUDRLUULULDRRDLLDRDRRULLLDDDRDUDLRDLRDDURRLDUDLLRUDRRRUDRURURRRRDRDDRULRRLLDDRRRLDLULRLRRRUDUDULRDLUDRULRRRRLUULRULRLLRLLURDLUURDULRLDLRLURDUURUULUUDRLLUDRULULULLLLRLDLLLDDDLUULUDLLLDDULRDRULURDLLRRDRLUDRD`;
+const INPUT = `\
+ULL
+RRDDD
+LURDL
+UUUUD`;
+
+const KEYPAD = `\
+  1
+ 234
+56789
+ ABC
+  D`;
 
 function processInput(input) {
     return input.split('\n').map(line => line.split(''));
 }
 
-function processLine(line, startingSpot) {
+function processLine(line, startingSpot, keypad) {
     return line.reduce(lineReducer, startingSpot);
 }
 
-function lineReducer(startingSpot, instruction) {
-    switch(instruction) {
+function findInKeypad(character, keypad) {
+    let line = 0;
+    let index = 0;
+    keypad.forEach((keypadLine, i) => {
+        const place = keypadLine.indexOf(character);
+        if (place !== -1) {
+            line = i;
+            index = place;
+        }
+    });
+    return { line, index };
+}
+
+function lineReducer(keypad, startingSpot, instruction) {
+    const spotClone = Object.assign({}, startingSpot);
+    switch (instruction) {
         case 'U':
-            if (startingSpot - 3 > 0) return startingSpot - 3;
+            if (startingSpot.line !== 0) spotClone.line -= 1;
             break;
         case 'R':
-            if (startingSpot % 3 !== 0) return startingSpot + 1;
+            if (keypad[startingSpot.line].length - 1 > startingSpot.index) spotClone.index += 1;
             break;
         case 'D':
-            if (startingSpot + 3 < 10) return startingSpot + 3;
+            if(keypad.length - 1 > startingSpot.line) spotClone.line += 1;
             break;
         case 'L':
-            if (startingSpot % 3 !== 1) return startingSpot - 1;
+            if (startingSpot.index !== 0) spotClone.index -= 1;
             break;
     }
-    return startingSpot;
+    const key = keypad[spotClone.line][spotClone.index];
+    console.log(startingSpot, instruction, spotClone, key);
+    return (key && key !== ' ') ? spotClone : startingSpot;
 }
 
-function findKeyCode(input) {
+function decipher(code, keypad) {
+    return code.map(location => keypad[location.line][location.index]).join('');
+}
+
+function findKeyCode(input, keypadInput) {
     const lines = processInput(input);
-    let startingSpot = 5;
+    const keypad = processInput(keypadInput);
+    const startingSpot = '5';
+    let start = findInKeypad(startingSpot, keypad);
+    const reducer = lineReducer.bind(null, keypad);
     const code = lines.map((line) => {
-        startingSpot = processLine(line, startingSpot);
-        return startingSpot;
+        start = line.reduce(reducer, start);
+        return start;
     });
-    return code.join('');
+    return decipher(code, keypad);
 }
 
-console.log(findKeyCode(INPUT));
+console.log(findKeyCode(INPUT, KEYPAD));
